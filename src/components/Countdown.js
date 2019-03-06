decc5989f9572928e9f23c4286dd54669ced0fc5
duedeu36 <duedeu36@gmail.com>
2019-03-06 16:06:30 +0100

    working on countdown timer

--- src/components/Countdown.js
+++ src/components/Countdown.js
@@ -2,35 +2,55 @@ import React, { Component } from 'react';
 
 class Countdown extends Component {
   state = {
-    countdown: 3
+    secondsCountdown: 120
   };
 
-  playSound = () => {
-    if (this.state.countdown == -1) {
-      alert('Game over! Next players turn.');
-      // var snd = new Audio("file.wav");
-      // snd.play();
+  getSeconds = () => {
+    return '0' + (this.state.secondsCountdown % 60);
+  };
+
+  getMinutes = () => {
+    return '0' + Math.floor(this.state.secondsCountdown / 60);
+  };
+
+  handleStartCounter = () => {
+    setInterval(function() {
       this.setState({
-        countdown: 3
+        secondsCountdown: -1
       });
-    }
+    }, 1000);
+  };
+
+  handleStopCounter = () => {
+    alert('stopping');
   };
 
   render() {
     return (
-      <div>
-        <h2 onChange={this.playSound()}>Time: {this.state.countdown}</h2>
+      <div className="container">
+        <div className="row">
+          <h2>
+            Time: {this.getMinutes().slice(-2)}:{this.getSeconds().slice(-2)}{' '}
+            min
+          </h2>{' '}
+          <button className="btn btn-danger" onClick={this.handleStartCounter}>
+            Start
+          </button>
+          <button className="btn btn-primary" onClick={this.handlerStopCounter}>
+            Stop
+          </button>
+        </div>
       </div>
     );
   }
 
-  componentDidMount() {
-    this.myInterval = setInterval(() => {
-      this.setState(prevState => ({
-        countdown: prevState.countdown - 1
-      }));
-    }, 1000);
-  }
+  // componentDidMount() {
+  //   this.myInterval = setInterval(() => {
+  //     this.setState(prevState => ({
+  //       countdown: prevState.countdown - 1
+  //     }));
+  //   }, 1000);
+  // }
 
   // componentWillUnmount() {
   //   clearInterval(this.myInterval);
